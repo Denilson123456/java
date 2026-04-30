314d80284c4be4cbc14074a5cb6a09a7c6a1a2fd
Denilson <ddsjr2004@gmail.com>
2026-04-29 21:41:39 -0300

    1

---
+++ Java/untitled/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class ptbr {
+    public static void main(String[] args) {
+        System.out.println("Alô Mundo!");
+    }
+}
+
+class spanish {
+    public static void main(String[] args) {
+        System.out.println("Ola Muendo!");
+    }
+}
