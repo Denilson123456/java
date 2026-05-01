49fff7fe31404e816d97dab72f4f6d897a4e0d65
Denilson <ddsjr2004@gmail.com>
2026-05-01 13:13:18 -0300

    Conversão de Celsius para Fahrenheit

---
+++ Java/untitled/src/ConvercaoFahrenheit.java
@@ -0,0 +1,15 @@
+public class ConvercaoFahrenheit {
+    public static void main (String[] args) {
+        // Definição das variáveis de temperatura e estrutura de mensagem.
+        double temperaturaCelsius = 23.4;
+        double temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
+        String mensagem = String.format("A temperatura atual em Celsius é %fº é equivalente a %fº em Fahrenheit.", temperaturaCelsius, temperaturaFahrenheit);
+
+        System.out.println(mensagem);
+
+        //Utilizando Casting para conversão dos tipos de váriaveis Double para int.
+        int temperaturaFahrenheitInteira = (int) temperaturaFahrenheit;
+        String mensagemConvertida = String.format("A temperatura em Fahrenheit convertida para inteiro é %dº", temperaturaFahrenheitInteira);
+        System.out.println(mensagemConvertida);
+    }
+}
