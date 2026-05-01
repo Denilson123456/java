086e41429918e40c95915ffc1ab6b74c4f2b4947
Denilson <ddsjr2004@gmail.com>
2026-05-01 18:40:05 -0300

    Corrigindo tipo de variáveis de entrada de String para double.

--- Java/untitled/src/ConvercaoFahrenheit.java
+++ Java/untitled/src/ConvercaoFahrenheit.java
@@ -5,11 +5,10 @@ public class ConvercaoFahrenheit {
         // Definição das variáveis de temperatura e estrutura de mensagem.
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite o valor da temperatura em Celsius: ");
-        String temperaturaCelsius = entrada.nextLine();
-        //Convertendo entrada para Double.
-        double celsius = Double.parseDouble(temperaturaCelsius);
-        double temperaturaFahrenheit =  (celsius * 1.8) + 32;
-        String mensagem = String.format("A temperatura atual em Celsius é %fº é equivalente a %fº em Fahrenheit.", celsius, temperaturaFahrenheit);
+        double temperaturaCelsius = entrada.nextDouble();
+
+        double temperaturaFahrenheit =  (temperaturaCelsius * 1.8) + 32;
+        String mensagem = String.format("A temperatura atual em Celsius é %fº é equivalente a %fº em Fahrenheit.", temperaturaCelsius, temperaturaFahrenheit);
 
         System.out.println(mensagem);
 
