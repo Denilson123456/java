5e82cc94dc192452397b754eded2e5501fcc846f
Denilson <ddsjr2004@gmail.com>
2026-05-02 12:53:14 -0300

    Simplificando programa com sistema de itinerações com For e Codicional com Switch case

--- Java/untitled/src/CalculoMediaFilmes.java
+++ Java/untitled/src/CalculoMediaFilmes.java
@@ -3,24 +3,65 @@ import java.util.Scanner;
 public class CalculoMediaFilmes {
     public static void main (String[] args) {
         Scanner entrada = new Scanner(System.in);
+        double nota = 0;
+        double mediaAvaliacao = 0;
 
-        //entrada do primeiro valor.
-        System.out.println("Digite o valor da primeira nota: ");
-        double notaUm = entrada.nextDouble();
+        System.out.println("""
+                *********************************************************
+                *Bem Vindo ao sistema de cálculo de média de avaliações!*
+                *********************************************************
+                \n
+                """);
+        System.out.println("Quantas avaliações serão cálculadas? ");
+        int quantidadeNotas = entrada.nextInt();
+        int quantidadeLoops = 1;
+        String numeroOrdinal = "nenhum";
 
-        //entrada do segundo valor.
-        System.out.println("Digite o valor da segunda nota: ");
-        double notaDois = entrada.nextDouble();
-        
-        //cálculo média.
-        int media = (int) (notaUm + notaDois) / 2;
-        String mensagem = String.format("""
-        ***************************
-        *A média das nota de Filme*
-        ***************************
-        \n
-        O resultado do cálculo da média das avaliações é: %d
-        """, media);
-        System.out.println(mensagem);
+
+
+        for (int i = 0; i < quantidadeNotas; i++)  {
+            switch (quantidadeLoops) {
+                case 1:
+                    numeroOrdinal = "primeira";
+                    break;
+                case 2:
+                    numeroOrdinal = "segunda";
+                    break;
+                case 3:
+                    numeroOrdinal = "terceira";
+                    break;
+                case 4:
+                    numeroOrdinal = "quarta";
+                    break;
+                case 5:
+                    numeroOrdinal = "quinta";
+                    break;
+                case 6:
+                    numeroOrdinal = "sexta";
+                    break;
+                case 7:
+                    numeroOrdinal = "sétima";
+                    break;
+                case 8:
+                    numeroOrdinal = "oitava";
+                    break;
+                case 9:
+                    numeroOrdinal = "nona";
+                    break;
+                case 10:
+                    numeroOrdinal = "décima";
+                    break;
+            }
+
+            System.out.println(String.format("Digite o valor da %s avaliação: ", numeroOrdinal));
+            nota = entrada.nextDouble();
+
+            mediaAvaliacao += nota;
+            quantidadeLoops+= 1;
+        }
+
+        System.out.println(String.format("O resultado da média das avaliações é: %.2f", mediaAvaliacao / quantidadeNotas ));
+
+
     }
 }
