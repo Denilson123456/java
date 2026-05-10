f17aaaf04059544326f105d968e7b916e5fe0baf
denilsonjr <ddsjr2004@gmail.com>
2026-05-10 20:21:25 -0300

    Try and cath adicionado

--- Java/untitled/src/CaixaEletronico.java
+++ Java/untitled/src/CaixaEletronico.java
@@ -1,25 +1,47 @@
 import java.util.Scanner;
 
 public class CaixaEletronico {
+
     static Scanner entrada = new Scanner(System.in);
     static double saldo = 1000;
-
 
     public static void verSaldo() {
         System.out.println(String.format("Seu saldo é: %.2f", saldo));
     }
+
     public static void realizarDeposito() {
+
         System.out.println("Digite o valor do Depósito: ");
-        double depositar = entrada.nextDouble();
-        saldo += depositar;
-        System.out.println(String.format("O novo Saldo após o depósito é: %.2f", saldo));
+
+        if (entrada.hasNextDouble()) {
+            double depositar = entrada.nextDouble();
+            saldo += depositar;
+
+            System.out.println(String.format(
+                    "O novo Saldo após o depósito é: %.2f", saldo));
+
+        } else {
+            System.out.println("Valor inválido!");
+            entrada.next(); // limpa entrada inválida
+        }
     }
 
-      public static void realizarSaque() {
+    public static void realizarSaque() {
+
         System.out.println("Digite o valor do Saque: ");
-        double saque = entrada.nextDouble();
-        saldo -= saque;
-        System.out.println(String.format("O novo Saldo após o saque é: %.2f", saldo));
+
+        if (entrada.hasNextDouble()) {
+            double saque = entrada.nextDouble();
+
+            saldo -= saque;
+
+            System.out.println(String.format(
+                    "O novo Saldo após o saque é: %.2f", saldo));
+
+        } else {
+            System.out.println("Valor inválido!");
+            entrada.next();
+        }
     }
 
     private static void Menu() {
@@ -31,30 +53,51 @@ public class CaixaEletronico {
                 """);
     }
 
-
     public static void main(String[] args) {
-        Scanner leitor  = new Scanner(System.in);
+
+        int numero = 0;
+
         System.out.println("""
               *********************************
               * Bem-vindo ao Caixa Eletrônico *
               *********************************
-              \n""");
-        int numero = 0;
+              """);
 
         while (numero != 4) {
+
             Menu();
+
             System.out.println("Escolha um número correspondente ao menu: ");
-            numero = leitor.nextInt();
-            switch (numero) {
-                case 1:
-                    verSaldo();
-                    break;
-                case 2:
-                    realizarDeposito();
-                    break;
-                case 3:
-                    realizarSaque();
-                    break;
+
+            if (entrada.hasNextInt()) {
+
+                numero = entrada.nextInt();
+
+                switch (numero) {
+
+                    case 1:
+                        verSaldo();
+                        break;
+
+                    case 2:
+                        realizarDeposito();
+                        break;
+
+                    case 3:
+                        realizarSaque();
+                        break;
+
+                    case 4:
+                        System.out.println("Programa encerrado.");
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+
+            } else {
+                System.out.println("Digite apenas números!");
+                entrada.next();
             }
         }
     }
